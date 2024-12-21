d03edfb67da3396a9f6fddd0e61bf8c1ceff09a9
Jeffrey Jose <jeffjosejeff@gmail.com>
2024-12-20 17:25:20 -0800

    dmesg

--- src/subcommands/dmesg.rs
+++ src/subcommands/dmesg.rs
@@ -1,7 +1,6 @@
-use crate::library::adb;
+use crate::subcommands::adb;
 use crate::types::DeviceDetails;
 use std::error::Error;
-use tokio::io::{AsyncBufReadExt, BufReader};
 
 pub struct DmesgArgs {
     pub device_id: Option<String>,
@@ -11,27 +10,16 @@ pub struct DmesgArgs {
 pub async fn run(
     args: DmesgArgs,
     target_device: &DeviceDetails,
-    host: &str,
-    port: &str,
+    _host: &str,
+    _port: &str,
 ) -> Result<(), Box<dyn Error>> {
-    let mut command = "dmesg".to_string();
+    let mut command = "shell dmesg".to_string();
     if !args.args.is_empty() {
         command = format!("{} {}", command, args.args.join(" "));
     }
 
-    execute_command(host, port, &command, Some(target_device)).await
-}
-
-async fn execute_command(
-    host: &str,
-    port: &str,
-    command: &str,
-    device: Option<&DeviceDetails>,
-) -> Result<(), Box<dyn Error>> {
-    let adb_id = device.map(|d| d.adb_id.as_str());
-
-    let response = adb::run_shell_command_async(host, port, command, adb_id).await?;
-
-    println!("{}", response);
-    Ok(())
+    adb::run(adb::AdbArgs {
+        command: &command,
+        adb_id: &target_device.adb_id,
+    }).await
 }
