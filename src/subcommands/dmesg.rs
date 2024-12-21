1dd28d9539004092fb0e0dff7171b76938cc46a8
Jeffrey Jose <jeffjosejeff@gmail.com>
2024-12-20 17:03:46 -0800

    stream output

--- src/subcommands/dmesg.rs
+++ src/subcommands/dmesg.rs
@@ -1,6 +1,7 @@
 use crate::library::adb;
 use crate::types::DeviceDetails;
 use std::error::Error;
+use tokio::io::{AsyncBufReadExt, BufReader};
 
 pub struct DmesgArgs {
     pub device_id: Option<String>,
@@ -28,7 +29,15 @@ async fn execute_command(
     device: Option<&DeviceDetails>,
 ) -> Result<(), Box<dyn Error>> {
     let adb_id = device.map(|d| d.adb_id.as_str());
-    let response = adb::run_shell_command_async(host, port, command, adb_id).await?;
-    println!("{}", response);
+    let mut command_vec = vec!["shell".to_string()];
+    command_vec.extend(command.split_whitespace().map(String::from));
+    
+    let stream = adb::run_command_stream(host, port, adb_id, &command_vec).await?;
+    let mut reader = BufReader::new(stream).lines();
+
+    while let Some(line) = reader.next_line().await? {
+        println!("{}", line);
+    }
+    
     Ok(())
 }
